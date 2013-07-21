8baad1a17c272e26df62cda2b600a455a54ae0cb
Maxim Dubinin <sim@gis-lab.info>
2013-07-22 02:07:00 +0400

    mode,median,age to total medals

--- analysis.r
+++ analysis.r
@@ -3,14 +3,33 @@
 # Modified: 0:38 16.07.2013
 # Analysis
 
-datafile = "d:\\Programming\\Python\\universiade\\all_data.csv"
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
 
+datafile = "d:\\Programming\\Python\\universiade2013\\data\\all_data.csv"
 d = read.csv(datafile)
-d = subset(d,d$CNTRY == c('RUS','USA','CHN','GER','GBR','FRA','JPN','KOR','CAN'))
+dy = data.frame(CNTRY=d$CNTRY,YEAR=as.numeric(as.character(d$DOB_YEAR)))
+dy = na.omit(dy)
+meanyear_all = aggregate(dy$YEAR, by=list(dy$CNTRY), FUN=mean)
 
-dd = data.frame(CNTRY=d$CNTRY,YEAR=as.numeric(as.character(d$DOB_YEAR)))
-#dd[complete.cases(dd),]
-dd = na.omit(dd)
+top10 = subset(d,d$CNTRY %in% c('RUS','USA','CHN','GER','GBR','FRA','JPN','KOR','CAN'))
+top10 = data.frame(CNTRY=top10$CNTRY,YEAR=as.numeric(as.character(top10$DOB_YEAR)))
+top10 = na.omit(top10)
 
-meanage = aggregate(dd$YEAR, by=list(dd$CNTRY), FUN=mean)
-barplot(meanage[,2] - mean(meanage[,2]),xlab = "Страна", ylab = "Отклонение от среднего, лет",names.arg=meanage[,1],ylim=c(-1,1))
+meanyear = aggregate(top10$YEAR, by=list(top10$CNTRY), FUN=mean)
+barplot(meanyear[,2] - mean(meanyear[,2]),xlab = "Страна", ylab = "Отклонение от среднего, лет",names.arg=meanyear[,1],ylim=c(-1,1))
+
+medianyear = aggregate(top10$YEAR, by=list(top10$CNTRY), FUN=median)
+barplot(medianyear[,2] - median(medianyear[,2]),xlab = "Страна", ylab = "Отклонение медианы от общей медианы, лет",names.arg=medianyear[,1],ylim=c(-2,2))
+
+modeyear = aggregate(top10$YEAR, by=list(top10$CNTRY), FUN=Mode)
+barplot(modeyear[,2] - mean(modeyear[,2]),xlab = "Страна", ylab = "Отклонение моды от общей моды, лет",names.arg=modeyear[,1],ylim=c(-2,2))
+
+
+mfile = "d:\\Programming\\Python\\universiade2013\\data\\medals_total.csv"
+m = read.csv(mfile)
+
+cntr_medals_all = merge(meanyear_all,m,by.x="Group.1",by.y="CNTRY")
+plot(2013-cntr_medals_all$x,cntr_medals_all$TOTAL,pch=19,xlab = "Средний возраст", ylab = "Общее количество медалей")
